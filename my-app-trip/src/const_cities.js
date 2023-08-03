7ecaad33357fcc97c5a20a57523fd7a599bd8964
luizagevorkian <66139931+luizagevorkian@users.noreply.github.com>
2023-08-03 21:48:19 +0200

    add new icon of app

---
+++ my-app-trip/src/const_cities.js
@@ -0,0 +1,12 @@
+export const cities = {
+    paris: "Paris",
+    vienna: "Vienna",
+    amsterdam: "Amsterdam",
+    berlin: "Berlin",
+    barcelona: "Barcelona",
+    kyiv: "Kyiv",
+    rome:"Rome",
+    istanbul: "Istanbul",
+    dubai: "Dubai",
+    newyork: "New York",
+  };
